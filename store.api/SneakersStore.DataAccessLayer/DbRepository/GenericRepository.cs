bc3db482c1fe7e060c884732f4230e1cc886b4f2
dilexet <63055058+dilexet@users.noreply.github.com>
2021-08-31 20:49:20 +0300

    Add rep

---
+++ store.api/SneakersStore.DataAccessLayer/DbRepository/GenericRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SneakersStore.DataAccessLayer.Abstract;
+using SneakersStore.DataAccessLayer.AppContext;
+
+namespace SneakersStore.DataAccessLayer.DbRepository
+{
+    public class GenericRepository : BaseRepository, IRepository
+    {
+        private StoreDbContext Context { get; }
+        private ILogger<GenericRepository> Logger { get; }
+
+        public GenericRepository(ILogger<GenericRepository> logger, string connectionString,
+            IContextFactory contextFactory)
+            : base(connectionString, contextFactory)
+        {
+            Context = contextFactory.CreateDbContext(connectionString);
+            Logger = logger;
+        }
+
+        public IEnumerable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> predicate)
+            where TEntity : class
+        {
+            if (predicate != null)
+            {
+                return Context.Set<TEntity>().Where(predicate);
+            }
+
+            return Context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> SingleOrDefault<TEntity>(Expression<Func<TEntity, bool>> predicate)
+            where TEntity : class
+        {
+            return await Context.Set<TEntity>().Where(predicate).SingleOrDefaultAsync();
+        }
+
+        public async Task<TEntity> Find<TEntity>(Guid? id) where TEntity : class
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Add<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity == null)
+            {
+                Logger.LogError("Attempt to add invalid data");
+                return;
+            }
+
+            Context.Set<TEntity>().AddAsync(entity);
+            // Context.SaveChangesAsync();
+        }
+
+        public async void Update<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity == null)
+            {
+                Logger.LogError("Attempt to add invalid data");
+                return;
+            }
+
+            // Context.Set<TEntity>().Update(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+            try
+            {
+                await Context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                // ReSharper disable once TemplateIsNotCompileTimeConstantProblem
+                Logger.LogError(ex.ToString());
+            }
+        }
+
+        public async void Remove<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity == null)
+            {
+                Logger.LogError("Attempt to remove invalid data");
+                return;
+            }
+
+            Context.Set<TEntity>().Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+        
+        public void RemoveRange<TEntity>(IEnumerable<TEntity> entity) where TEntity : class
+        {
+            if (entity == null)
+            {
+                Logger.LogError("Attempt to remove invalid data");
+                return;
+            }
+
+            Context.Set<TEntity>().RemoveRange(entity);
+        }
+
+        public async void Save()
+        {
+            await Context.SaveChangesAsync();
+        }
+    }
+}
---
+++ store.api/SneakersStore.WebAPI/Factory/DesignTimeRepositoryContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SneakersStore.DataAccessLayer.AppContext;
+using SneakersStore.DataAccessLayer.Factory;
+
+namespace SneakersStore.WebAPI.Factory
+{
+    public class DesignTimeRepositoryContextFactory :
+        IDesignTimeDbContextFactory<StoreDbContext>
+    {
+        public StoreDbContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("StoreConnectionString");
+            var repositoryFactory = new StoreContextFactory();
+
+            return repositoryFactory.CreateDbContext(connectionString);
+        }
+    }
+}
